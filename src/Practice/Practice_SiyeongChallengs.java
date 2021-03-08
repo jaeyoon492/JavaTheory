0504116c8e3e9349c70128c7cf24abcc3fa32389
JaeYoonMyung <77507968+jaeyoon492@users.noreply.github.com>
2021-03-08 11:38:29 +0900

    Update Practice_SiyeongChallengs.java

--- src/Practice/Practice_SiyeongChallengs.java
+++ src/Practice/Practice_SiyeongChallengs.java
@@ -6,48 +6,53 @@ import java.util.stream.Stream;
 
 public class Practice_SiyeongChallengs {
     public static void main(String[] args) {
-        int[] answer = {(int)(Math.random()*9)+1, (int)(Math.random()*9), (int)(Math.random()*9)};
-
-        String number = new String();
-        int[] num = new int [3];
-        int failcount = 0;
-        int count = 0;
-        int input = 0;
+        int[] answer = {(int)(Math.random()*9)+1, (int)(Math.random()*9), (int)(Math.random()*9)}; //3자리 난수를 받는다.
 
         System.out.println(Arrays.toString(answer));
+        int failcount = 0; //실패시 카운트
+        int count = 0;     //성공시 카운트
+        int howMany = 0;
+        String input;      //입력받은 숫자를 저장하기 위한.
         Scanner sc = new Scanner(System.in);
+        
 
         do {
-            System.out.print("세자리의 숫자를 입력하세요");
+            System.out.print("세자리 숫자를 입력하세요");
+            howMany++;
+            input = sc.nextLine();
+            Integer.parseInt(input); //저장한 문자를 숫자로 전환.
+            int[] guess = {input.charAt(0)-48,input.charAt(1)-48,input.charAt(2)-48}; //3자리수로 받아서 분할후 배열화
 
-            for (int i = 0 ; i < num.length; i++){
-                num[i] = sc.nextInt();
-            }
 
-            if(num[0] == answer[0]) {
+            if(answer[0] == guess[0]) {
                 count++;
-            }else if (num[0] != answer[0]) {
+            }else if (answer[0] != guess[0]) {
                 failcount++;
             }
 
-            if(num[1] == answer[1]) {
+            if(answer[1] == guess[1]) {
                 count++;
-            }else if (num[1] != answer[1]) {
+            }else if (answer[1] != guess[1]) {
                 failcount++;
             }
 
-            if(num[2] == answer[2]) {
+            if(answer[2] == guess[2]) {
                 count++;
-            }else if (num[2] != answer[2]) {
+            }else if (answer[2] != guess[2]) {
                 failcount++;
             }
-            if(num[0] != answer[0]){
+
+            if(answer[0] == guess[0] && answer[1] == guess[1] && answer[2] == guess[2]){
+                System.out.println("정답!");
+                System.out.println("시도하신 횟수는" + howMany + "입니다.");
+                break;
+            } else if (answer[0] != guess[0] || answer[1] != guess[1] || answer[2] != guess[2]){
+                System.out.println("총" + count + "개 맞았고" + failcount + "개 틀렸습니다.");
                 count = 0;
-            }else{
-                failcount = 0;
+            failcount = 0;
             }
 
-            System.out.println("총" + count + "개 맞았고" + failcount + "개 틀렸습니다.");
+
 
         }while(true);
 
