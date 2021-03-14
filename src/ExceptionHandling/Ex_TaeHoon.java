60b9860455dde74832b112d3ee992cef3f16aef8
JaeYoonMyung <77507968+jaeyoon492@users.noreply.github.com>
2021-03-14 21:38:23 +0900

    Exception

---
+++ src/ExceptionHandling/Ex_TaeHoon.java
@@ -0,0 +1,34 @@
+package ExceptionHandling;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Ex_TaeHoon {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자를 입력하세요");
+        String str = sc.nextLine();
+
+        int i;
+        try {
+            i = Integer.parseInt(str);
+        } catch (Exception e) {
+            System.out.println("숫자를 넣어야지 문자를 왜 넣냐 0 받아라");
+            i = 0;
+        }
+
+        System.out.println("입력받은 숫자 : " + i);
+
+//        int i;
+//        if (str.matches("^[0-9]+$")){   // 어쨋든 숫자일때 true 라는 조건임
+//            i = Integer.parseInt(str);
+//        }else{
+//            System.out.println("숫자 입력안했으니까 0 드림");
+//            i = 0;
+//        }
+//
+//        System.out.println("입력받은 숫자 : " + i);
+
+    }
+}
