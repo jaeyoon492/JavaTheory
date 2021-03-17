7314fde543424064d2a79ddd8f98e1c68bf0bc9b
JaeYoonMyung <77507968+jaeyoon492@users.noreply.github.com>
2021-03-17 23:16:18 +0900

    Create Quiz8_7.java

---
+++ src/ExceptionHandling/Quiz8_7.java
@@ -0,0 +1,30 @@
+package ExceptionHandling;
+import java.util.*;
+public class Quiz8_7 {
+    public static void main(String[] args) {
+        int answer = (int) (Math.random() * 100)+1;
+        int input = 0;
+        int count = 0;
+
+        do {
+            count++;
+            System.out.println("1과 100사이의 값을 입력하세요. : ");
+
+           try {
+               input = new Scanner(System.in).nextInt();
+           } catch (Exception e) {
+               System.out.println("숫자를 입력하세요.");
+           }
+
+            if(answer > input) {
+                System.out.println("더 큰수를 입력하세요.");
+            } else if(answer < input) {
+                System.out.println("더 작은수를 입력하세요.");
+            } else {
+                System.out.println("맞췄습니다.");
+                System.out.println("시도횟수는" + count + "번 입니다,");
+                break;
+            }
+        } while (true);
+    }
+}
