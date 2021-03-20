d48790fc02ca8dff5223abc7c13bfa874678b7c3
JaeYoonMyung <77507968+jaeyoon492@users.noreply.github.com>
2021-03-20 12:29:25 +0900

    3/20 Object

--- src/Object/Quiz6_18.java
+++ src/Object/Quiz6_18.java
@@ -1,14 +1,14 @@
-package Object;
+//package Object;
 
-public class Quiz6_18 {
-    static boolean isNumber(String str){
-        boolean yes = true;
-    }
-    public static void main(String[] args) {
-        String str = "123";
-        System.out.println(str + "는 숫자입니까?" + isNumber(str));
-
-        str = "1234o";
-        System.out.println(str + "는 숫자입니까?" + isNumber(str));
-    }
-}
+//public class Quiz6_18 {
+//    static boolean isNumber(String str){
+//        boolean yes = true;
+//    }
+//    public static void main(String[] args) {
+//        String str = "123";
+//        System.out.println(str + "는 숫자입니까?" + isNumber(str));
+//
+//        str = "1234o";
+//        System.out.println(str + "는 숫자입니까?" + isNumber(str));
+//    }
+//}
