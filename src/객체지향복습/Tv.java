c1b4ac841febad26c09bee8e424512bf21ab889e
JaeYoonMyung <77507968+jaeyoon492@users.noreply.github.com>
2021-05-24 18:06:23 +0900

    객체지향복습

---
+++ src/객체지향복습/Tv.java
@@ -0,0 +1,17 @@
+package 객체지향복습;
+
+public class Tv {
+    String color;
+    boolean power;
+    int channel;
+
+    void power(){
+        power = !power;
+    }
+    void channelUp(){
+        ++channel;
+    }
+    void channelDown(){
+        --channel;
+    }
+}
