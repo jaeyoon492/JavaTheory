59939b1241b064be07a58f1bef2c81eb4510fecf
JaeYoonMyung <77507968+jaeyoon492@users.noreply.github.com>
2021-07-03 21:13:13 +0900

    Update Ex7_3.java

--- src/Object/Ex7_3.java
+++ src/Object/Ex7_3.java
@@ -7,14 +7,15 @@ public class Ex7_3 {
     }
 }
 
-class Parent2{
+class Parent2 {
     int x = 10;
 }
 
-class Child2 extends Parent2{
+class Child2 extends Parent2 {
     void method() {
         System.out.println("x=" + x);
         System.out.println("this.x=" + this.x);
         System.out.println("super.x=" + super.x);
     }
 }
+
