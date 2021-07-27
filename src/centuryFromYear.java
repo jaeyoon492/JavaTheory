8583a07246353a5e39e9a0e30a924bb60451e063
JaeYoonMyung <77507968+jaeyoon492@users.noreply.github.com>
2021-07-28 00:44:54 +0900

    codingTestCentury

---
+++ src/centuryFromYear.java
@@ -0,0 +1,16 @@
+public class centuryFromYear {
+    int centuryFromYears(int year) {
+        if (year % 100 != 0){
+            return year / 100 + 1;
+        }else{
+            return year / 100;
+        }
+    }
+
+    public static void main(String[] args) {
+        centuryFromYear centuryFromYear = new centuryFromYear();
+        int num = centuryFromYear.centuryFromYears(10);
+        System.out.println(num);
+    }
+}
+
